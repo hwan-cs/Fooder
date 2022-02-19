736acda913d9214d5554a72b23ad05ff263b460a
hwan-cs <hwann1005@gmail.com>
2022-02-19 23:03:46 +0900

    Test commit

--- Fooder/ViewController.swift
+++ Fooder/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        print("Test commit")
+        print("Test123123")
         // Do any additional setup after loading the view.
     }
 

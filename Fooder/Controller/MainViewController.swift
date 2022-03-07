b1a31e18162ffe3a0d1294ba7390cdc2e79fe0d6
hwan-cs <hwann1005@gmail.com>
2022-03-07 16:36:43 +0900

    Searchbar animation

--- Fooder/Controller/MainViewController.swift
+++ Fooder/Controller/MainViewController.swift
@@ -10,26 +10,62 @@ import GooglePlaces
 
 class MainViewController: UIViewController
 {
-    @IBOutlet var searchBar: UISearchBar!
+    private var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var searchButton: UIButton!
     
     private var tableDataSource: GMSAutocompleteTableDataSource!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        view.backgroundColor = K.mainBgColor
+        searchBar = UISearchBar(frame: CGRect(x: 0, y: self.view.frame.size.height/2, width: self.view.frame.size.width, height: 44.0))
         searchBar.delegate = self
+        view.addSubview(searchBar)
         
         tableDataSource = GMSAutocompleteTableDataSource()
         tableDataSource.delegate = self
         
         tableView.delegate = tableDataSource
         tableView.dataSource = tableDataSource
+        
+        let largeConfig = UIImage.SymbolConfiguration(pointSize: 40, weight: .bold, scale: .large)
+        let img = UIImage(systemName: "magnifyingglass", withConfiguration: largeConfig)
+        searchButton.setImage(img, for: .normal)
+        
+        let filter = GMSAutocompleteFilter()
+        filter.type = .establishment
+        filter.country = "kr"
+        tableDataSource.autocompleteFilter = filter
+        
+        searchBar.searchTextField.backgroundColor = .gray
+        searchBar.barTintColor = UIColor.clear
+        searchBar.backgroundImage = UIImage()
+        
+        searchBar.alpha = 0
+        tableView.alpha = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.searchBar.endEditing(true)
+    }
+    
+    @IBAction func searchClicked(_ sender: UIButton)
+    {
+        UIView.animate(withDuration: 0.5)
+        {
+            self.searchButton.alpha = 0
+            self.searchBar.alpha = 1
+            self.searchBar.frame.origin.y = 50
+        }
     }
 }
 
 extension MainViewController: UISearchBarDelegate
 {
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String)
     {
         tableDataSource.sourceTextHasChanged(searchText)
